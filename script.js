fbd41d6ef92eccd7df28a8af13e93ac4fe622808
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-03-13 18:40:51 +0300

    updated

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ let laptop1 = {
     year: 2018,
     kg: 1.2,
     price: 1000,
+    explanation : function(){
+        return `${this.brand.toUpperCase()} ${this.model} is a ${this.year} model and it is ${this.kg} kg and it costs ${this.price} dollars.`;
+    }
 }
 console.log(laptop1);
 console.log(laptop1.brand);
-
+console.log(laptop1.explanation());
+console.log("----------------------------");
+laptop1.explanation()
+console.log("----------------------------");
 for(let key in laptop1){
     console.log(key + " : " + laptop1[key]);
    // console.log(laptop1[key]);
@@ -29,3 +35,18 @@ console.log(laptop2);
 let laptop3 = new Laptop("Dell", "Inspiron", 2017, 2.0, 800);
 console.log(laptop3);
  
+
+keys = Object.keys(laptop1);
+//console.log(keys);
+console.log(Object.keys(laptop1)); // returns an array of the keys (property names) of all enumerable properties of the laptop1 object.
+console.log(Object.values(laptop1)); //returns an array of the values of all enumerable properties of the laptop1 object.
+
+keys.forEach(keyI => {
+    console.log(keyI + " : " + laptop1[keyI]);
+    // console.log(key);
+   // console.log(laptop1[key]);
+});
+
+function yaz(){console.log("Merhaba Kodluyoruz")}
+console.log(yaz["name"]); //returns yaz function name
+console.log(yaz.name); //returns yaz function name
